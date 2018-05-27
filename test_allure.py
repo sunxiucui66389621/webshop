7ef7fc1db7050469148c0bc8f28a1706a9ab0295
webshop <66389621@qq.com>
2018-05-27 16:37:54 +0800

    步骤描述内容

--- test_allure.py
+++ test_allure.py
@@ -1,10 +1,10 @@
-import pytest
+import pytest,allure
 class Test_001:
     @allure.step(title="测试步骤01")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_a(self,a):
         assert  a == 2
     @allure.step(title="测试步骤02")
-    @pytest.mark.parametrize("b,[4,5,6]")
+    @pytest.mark.parametrize("b",[4,5,6])
     def test_b(self,b):
         assert b !=5
